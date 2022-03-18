512bfe31e63e290c7341e762b02c9827f9eb502c
Peyman Ahmadi <peyman.ahmadi@outlook.com>
2022-03-18 15:01:25 +0330

    Connect Backend to MongoDB

--- pishgam-backend/app.js
+++ pishgam-backend/app.js
@@ -1,9 +1,9 @@
 const express = require("express");
-
-const HttpError = require("./models/http-error");
+const mongoose = require("mongoose");
 
 const thingsRoutes = require("./routes/things-routes");
 const usersRoutes = require("./routes/users-routes");
+const HttpError = require("./models/http-error");
 
 const app = express();
 
@@ -25,4 +25,11 @@ app.use((error, req, res, next) => {
   res.json({ message: error.message || "An unknown error occured!" });
 });
 
-app.listen(5000);
+mongoose
+  .connect("mongodb://localhost:27017/pishgam")
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
