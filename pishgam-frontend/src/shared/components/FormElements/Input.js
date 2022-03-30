b2829796a8d9bfd7dbf2c7845b963854c5c277ff
Peyman Ahmadi <peyman.ahmadi@outlook.com>
2022-03-30 10:10:16 +0430

    Create Input Module

---
+++ pishgam-frontend/src/shared/components/FormElements/Input.js
@@ -0,0 +1,18 @@
+import styles from "./Input.module.scss";
+
+const Input = (props) => {
+  const element =
+    props.element === "input" ? (
+      <input id={props.id} type={props.type} placeholder={props.placeholder} />
+    ) : (
+      <textarea id={props.id} rows={props.rows || 3} />
+    );
+  return (
+    <div className={styles["from-control"]}>
+      <label htmlFor={props.id}>{props.label}</label>
+      {element}
+    </div>
+  );
+};
+
+export default Input;
